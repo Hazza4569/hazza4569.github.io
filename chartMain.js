9fea54bfa0d309cc0fba25a286e9fd4bafaa6780
Hazza4569 <hcooke006@aol.com>
2017-08-22 10:30:43 +0100

    Multiplots

--- chartMain.js
+++ chartMain.js
@@ -1,6 +1,7 @@
 var w = 640; var h = 360; // set width and height of video
 
 var colour = 'cyan'; //set theme colour
+var colour2 = 'magenta';
 document.getElementsByTagName("h1")[0].style.color = colour;
 
 var v = document.getElementById("vid"); 
@@ -28,7 +29,8 @@ var e = document.getElementById("myList");
 var L = document.getElementById("left");
 var bkg = document.getElementById("white");
 
-var numPoints = 60;	//set number of points to plot curve in
+
+var numPoints = 40;	//set number of points to plot curve in
 //(higher number for better res, lower for better performance)
 
 var xscale = 2.13; //empirical values needed to scale drawings to correct size,
@@ -37,7 +39,7 @@ var arrScale = 35 * numPoints/360; //scale size of velocity arrows
 
 var t = 1; //initialise counter
 var x,y; 
-var vx, vy;
+var vx, vy,ax,ay;
 
 window.addEventListener( 'resize', onWindowResize, false );
 v.addEventListener('ended',onVidEnd,false);
@@ -59,7 +61,7 @@ function init()
     	h = 360;
     }
 
-	[x,y,vx,vy] = getPoints();
+	[x,y,vx,vy,ax,ay] = getPoints();
 
 	ctx.clearRect(0,0,theCanvas.width,theCanvas.height); //clear canvases on resize to prevent odd shapes
 	cty.clearRect(0,0,comCanvas.width,comCanvas.height);
@@ -117,14 +119,8 @@ function init()
     cntHeight += 45;
     graph.style.top = cntHeight + "px";
 
-    cntHeight += 320 + 25;
-    txt.style.top = cntHeight + "px";
-    var txtWidth = (w);
-    txt.style.width = txtWidth.toString() + "px";
-    txt.style.padding = "10px " + (pad).toString() + "px";
+    txtInit();
 
-    suvat.style.width = (377*w/640).toString() + "px";
-    suvat.style.height = (165*w/640).toString() + "px";
 
     var rWidth = Math.max(0.8*pad - 10,0)
 
@@ -143,73 +139,188 @@ function init()
     title.style.height="70px";
     title.style.top="20px";
 
-    GRAPHS(); //Make graph
+    //GRAPHS(); //Make graph
+}
 
+function txtInit()
+{
+    var pad = ((window.innerWidth-w)/2);
+    cntHeight = 15 + h + 80 - 5 + 45;
+
+    if (btn.disabled) {cntHeight+=340}
+
+    txt.style.top = cntHeight + "px";
+    var txtWidth = (w);
+    txt.style.width = txtWidth.toString() + "px";
+    txt.style.padding = "10px " + (pad).toString() + "px";
+
+    suvat.style.width = (377*w/640).toString() + "px";
+    suvat.style.height = (165*w/640).toString() + "px";
 }
 
 function Run()
 {
-	btn.disabled = true;
-	btn.style.backgroundColor = '#cccccc';
-	ctx.clearRect(0,0,theCanvas.width,theCanvas.height);
-	Plotly.purge('graph');
-	v.play();
-	t=1;
-	GRAPHS();
-	animate();
+    if (btn.value=="Analyse")
+    {
+        analyse();
+    }
+    else
+    {
+        throwBall();
+    }
 };
+
+function throwBall()
+{
+    btn.value = "Analyse";
+    ctx.clearRect(0,0,theCanvas.width,theCanvas.height);
+    Plotly.purge('graph');
+    txtInit();
+    v.play();
+}
+
+function analyse()
+{
+    btn.disabled = true;
+    btn.style.backgroundColor = '#cccccc';
+    txtInit();
+    GRAPHS();
+    t=1;
+    animate();
+}
 
 function animate() //animate loop works recursively without a setInterval, based on advancement of video 
 {
 	ballPos = (147.7*v.currentTime-66.33); //position of ball in pixels as function of video time
 	if (t < ballPos*numPoints/640) //NB: numpoints/640 is conversion from pixels to points plotted over
 	{
-		ctx.beginPath();
-		ctx.moveTo(x[t-1],y[t-1]);
-		ctx.lineTo(x[t],y[t]);
-		ctx.stroke();
-		components();
-		updateGraph();
+        if (btn.disabled)
+        {
+		  ctx.beginPath();
+		  ctx.moveTo(x[t-1],y[t-1]);
+		  ctx.lineTo(x[t],y[t]);
+		  ctx.stroke();
+		  components();
+		  updateGraph();
+        }
 		t++;
 	}
 
 	if (t<=numPoints+2)
 	{
-		setTimeout(animate,0.1); //setTimeout is only required to prevent too much unnecessary recursion
+		setTimeout(animate,0.001); //setTimeout is only required to prevent too much unnecessary recursion
 	}
+    else
+    {
+        cty.clearRect(0,0,comCanvas.width,comCanvas.height);
+        ctz.clearRect(0,0,comCanvas2.width,comCanvas2.height);
+        btn.disabled=false;
+        btn.value="Throw!"
+        btn.style.backgroundColor = colour;
+    }
 }
 
 function GRAPHS()
 {
 	var f = x;
-	var g, yrange;
-	[g,yrange] = choice(e.options[e.selectedIndex].value,0);
+	var h, g, yrange;
+    var h2, g2, yrange2;
+    var h3, g3, yrange3;
+	[g,h,yrange] = choice2('Distance',0);
+    [g2,h2,yrange2] = choice2('Velocity',0);
+    [g3,h3,yrange3] = choice2('Acceleration',0);
 
- 	Plotly.plot('graph', [       //graph plotted using plotly.js
- 	{
-   		y: [[g]],
-   		x: [f[0]],
-   		mode: 'lines',
-   		line: {color: colour}
-	}], 
+    var traceDistY = {
+        y: [[g]],
+        x: [f[0]],
+        mode: 'lines',
+        line: {color: colour},
+        name: 'Vertical'
+    };
+
+    var traceDistX = {
+        y: [[h]],
+        x: [f[0]],
+        mode: 'lines',
+        line: {color: colour2},
+        name: 'Horizontal'
+    };
+
+    var traceVelY = {
+        y: [[g2]],
+        x: [f[0]],
+        xaxis: 'x2',
+        yaxis: 'y2',
+        mode: 'lines',
+        line: {color: colour},
+        showlegend: false
+        //name: 'Vertical'
+    };
+
+    var traceVelX = {
+        y: [[h2]],
+        x: [f[0]],
+        mode: 'lines',
+        xaxis: 'x2',
+        yaxis: 'y2',
+        line: {color: colour2},
+        showlegend: false
+        //name: 'Horizontal'
+    };
+
+    var traceAccY = {
+        y: [[g3]],
+        x: [f[0]],
+        xaxis: 'x3',
+        yaxis: 'y3',
+        mode: 'lines',
+        line: {color: colour},
+        showlegend: false
+        //name: 'Vertical'
+    };
+
+    var traceAccX = {
+        y: [[h3]],
+        x: [f[0]],
+        mode: 'lines',
+        xaxis: 'x3',
+        yaxis: 'y3',
+        line: {color: colour2},
+        showlegend: false
+        //name: 'Horizontal'
+    };
+
+
+
+ 	Plotly.plot('graph', [traceDistY,traceDistX,traceVelX,traceVelY,traceAccX,traceAccY], 
 	{
 		margin: {t:10,l:20,r:10,b:20},
-		xaxis: {range: [0,320], showticklabels: false,title: 'time'},
-		yaxis: {range: yrange, showticklabels: false, title: e.options[e.selectedIndex].value},
-		showlegend: false
+		xaxis: {domain: [0,1], range: [0,320], showticklabels: false,title: 'time'},
+		yaxis: {domain: [0,0.45], range: yrange, showticklabels: false, title: 'Distance'},
+        xaxis2: {domain: [0,0.45], anchor: 'y2', range: [0,320], showticklabels: false},
+        yaxis2: {domain: [0.55,1], anchor: 'x2', range: yrange2, showticklabels: false, title: 'Velocity'},
+        xaxis3: {domain: [0.55,1], anchor: 'y3', range: [0,320], showticklabels: false},
+        yaxis3: {domain: [0.55,1], anchor: 'x3', range: yrange3, showticklabels: false, title: 'Acceleration'},
+		showlegend: true, legend: {x:0.8,y:0.58},
+        showgrid: true
 	});
+    console.log(ax,ay)
 }
 
 function updateGraph()
 {
 	var f = x;
-	var g, yrange;
-	[g,yrange] = choice(e.options[e.selectedIndex].value,t); //gets value to plot from drop-down box (choice fn at bottom)
-  	Plotly.extendTraces('graph',
+	var h, g, yrange;
+    var h2, g2, yrange2;
+    var h3, g3, yrange3;
+    [g,h,yrange] = choice2('Distance',t);
+    [g2,h2,yrange2] = choice2('Velocity',t);
+    [g3,h3,yrange3] = choice2('Acceleration',t);
+    Plotly.extendTraces('graph', 
    	{
-   	    y: [[g]],
-   	    x: [[f[t]]]
-   	}, [0])
+   	    y: [[g],[h],[g2],[h2],[g3],[h3]], 
+   	    x: [[f[t]],[f[t]],[f[t]],[f[t]],[f[t]],[f[t]]]
+   	}, [0,1,2,3,4,5])
 }
 
 function components()
@@ -243,10 +354,7 @@ function arrow(context, fromx, fromy, tox, toy)
 
 function onVidEnd(e)
 {
-	btn.disabled=false;
-	btn.style.backgroundColor = colour;
-	cty.clearRect(0,0,comCanvas.width,comCanvas.height);
-	ctz.clearRect(0,0,comCanvas2.width,comCanvas2.height);
+	
 }
 
 function onWindowResize() {
@@ -259,6 +367,8 @@ function getPoints()
 	var y = [];
 	var vx = [];
 	var vy = [];
+    var ax = [];
+    var ay = [];
 	x[0] = 0;
 	y[0] = 296/yscale;
 	for (var i= 1; i <= numPoints+2; i++)
@@ -272,10 +382,21 @@ function getPoints()
 
 		vx[i] = x[i]-x[i-1];
 		vy[i] = y[i]-y[i-1];
+
 	}
 	vy[0] = vy[1];
 	vx[0] = vx[1];
-	return [x,y,vx,vy];
+
+    for (var i= 1; i <= numPoints+2; i++)
+    {
+        ax[i] = 0
+        ay[i] = vy[i]-vy[i-1];
+    }
+    ax[0]=ax[1];
+    ay[1]=ay[2];
+    ay[0]=ay[1];
+
+	return [x,y,vx,vy,ax,ay];
 }
 
 function choice(i,j) //returns value to plot for given drop down choice, and range of plot
@@ -295,5 +416,21 @@ function choice(i,j) //returns value to plot for given drop down choice, and ran
     else if (i=="X coordinate")
     {
         return  [x[j],[0,320]];
+    }
+}
+
+function choice2(i,j)
+{
+    if (i=="Distance")
+    {
+        return [(150-y[j])*2,x[j],[0,320]]
+    }
+    else if (i=="Velocity")
+    {
+        return [-vy[j],vx[j],[-10,10]]
+    }
+    else if (i=="Acceleration")
+    {
+        return [-ay[j],ax[j],[-0.5,0.05/Math.pow(numPoints/60,3)]]
     }
 }
