18aac89b92a0e7b01149c205c6db65b531b846ce
Hazza4569 <hcooke006@aol.com>
2022-10-12 15:06:43 +0100

    time update

--- vivasweep/main.js
+++ vivasweep/main.js
@@ -49,7 +49,7 @@ function startTime()
 {
     //Set time:
     let start = new Date('12 Oct 2022 11:57:00 GMT');
-    let stop = new Date('12 Oct 2022 11:56:00 GMT');
+    let stop = new Date('12 Oct 2022 19:00:00 GMT');
     let now = new Date();
     let duration = new Date(now.getTime() - start.getTime() - 3600*1000);
     let negative = duration.getTime() + 3600*1000 < 0
